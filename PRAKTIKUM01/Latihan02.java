bb5422ed4a2011d37e74f7d2e5eb87d0c75105eb
adeliarztr <adeliarizkiekaputr@gmail.com>
2024-09-06 09:34:17 +0700

    commit praktim 01

---
+++ PRAKTIKUM01/Latihan02.java
@@ -0,0 +1,16 @@
+/**
+ * Latihan02
+ */
+public class Latihan02 {
+
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama    :Adelia Rizki Eka Putri");
+        System.out.println("Absen   :02");
+        System.out.println("Kelas   :1D");
+        System.out.println("Prodi   :Sistem Informasi Bisnis");
+        System.out.println("Jurusan :Teknologi Informasi");
+        System.out.println("Alamat  :Singosari");
+        System.out.println("No Hp   :08813154922");
+    }
+}
---
+++ PRAKTIKUM01/MyFirstJava02.java
@@ -0,0 +1,10 @@
+/**
+ * MyFirstJava02
+ */
+public class MyFirstJava02 {
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Adelia Rizki Eka Putri");
+        System.out.print("saya adalah Mahasiswa Jurusan Teknologi Informasi-POLINEMA"
+        
+    }
+}
